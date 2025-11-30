f21bc2616360e93267a1830329887864fea13763
adi <adi.benh9@gmail.com>
2025-11-30 17:34:12 +0100

    Correcte databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(2)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        Customer::factory()->count(50)->create();
+
+        $Admin = User::create([
+            'name' => 'Admin',
             'email' => 'test@example.com',
+            'password' => bcrypt('secret'),
         ]);
+
+
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'finance']);
+        Role::create(['name' => 'sales']);
+        Role::create(['name' => 'inkoop']);
+        Role::create(['name' => 'maintenance']);
+
+
+        $Admin->assignRole('Admin');
     }
 }
